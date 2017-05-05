0598eed3f19172d07e3f463f237aa4bfd527766e
SIBON <J7552815@sg.net>
2017-05-05 10:26:00 +0200

    using silex

---
+++ web0/csv.php
@@ -0,0 +1,25 @@
+<?php
+    header('Expires: Sat, 26 Jul 1997 05:00:00 GMT');
+    header('Last-Modified: ' . gmdate( 'D, d M Y H:i:s') . ' GMT');
+    header('Cache-Control: no-store, no-cache, must-revalidate');
+    header('Cache-Control: post-check=0, pre-check=0', false);
+    header('Pragma: no-cache'); 
+
+$File = 'https://docs.google.com/spreadsheets/d/1s10qJviUHayRFRHxSbMGNDKaIg7-gyYAjz6kOPhPm6g/pub?gid=0&single=true&output=csv';
+
+https://docs.google.com/spreadsheets/d/1s10qJviUHayRFRHxSbMGNDKaIg7-gyYAjz6kOPhPm6g/pubhtml
+//https://docs.google.com/spreadsheets/d/1s10qJviUHayRFRHxSbMGNDKaIg7-gyYAjz6kOPhPm6g/pub?gid=910167407&single=true&output=csv
+//https://docs.google.com/spreadsheets/d/1s10qJviUHayRFRHxSbMGNDKaIg7-gyYAjz6kOPhPm6g/pub?gid=1976579302&single=true&output=csv
+
+
+
+$handle     = fopen($File, "r");
+if(empty($handle) === false) {
+    while(($data = fgetcsv($handle, 1000, ",")) !== FALSE){
+        echo $data[0] . "," . $data[1] . "," . $data[2] . "," . $data[3] . "," . $data[4] . "," . $data[5] . ";";
+
+    }
+    fclose($handle);
+}
+
+?>
