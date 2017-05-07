48f334af67714f5e6e47e80008db390acdd52a49
Jean-Christophe Sibon <jcsibon@gmail.com>
2017-05-07 16:23:04 +0200

    using silex

--- web/index.php
+++ web/index.php
@@ -24,7 +24,7 @@ $app->get('/catalog', function() use($app) {
   $keys = array_shift($file);
   foreach ($file as $i=>$row) {
       $row = array_combine($keys, $row);
-      $catalog[$data['universeUrlname']]=$data;
+      $catalog[$row['universeUrlname']]=$row;
   }
 
   $file = array_map("str_getcsv", file("https://docs.google.com/spreadsheets/d/1s10qJviUHayRFRHxSbMGNDKaIg7-gyYAjz6kOPhPm6g/pub?gid=1183165030&single=true&output=csv",FILE_SKIP_EMPTY_LINES));
