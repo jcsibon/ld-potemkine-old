24c7ad9976663b5ec2dab331bb80d76b3ec1796e
Jean-Christophe Sibon <jcsibon@gmail.com>
2017-05-07 16:34:45 +0200

    using silex

--- web/index.php
+++ web/index.php
@@ -13,7 +13,37 @@ $app->register(new Silex\Provider\TwigServiceProvider(), array(
     'twig.path' => __DIR__.'/views',
 ));
 
+$file = array_map("str_getcsv", file("https://docs.google.com/spreadsheets/d/1s10qJviUHayRFRHxSbMGNDKaIg7-gyYAjz6kOPhPm6g/pub?gid=1971894571&single=true&output=csv",FILE_SKIP_EMPTY_LINES));
+$keys = array_shift($file);
+foreach ($file as $i=>$row) {
+    $row = array_combine($keys, $row);
+    $app['catalog'][$row['universeUrlname']]=$row;
+}
+
+$file = array_map("str_getcsv", file("https://docs.google.com/spreadsheets/d/1s10qJviUHayRFRHxSbMGNDKaIg7-gyYAjz6kOPhPm6g/pub?gid=1121908549&single=true&output=csv",FILE_SKIP_EMPTY_LINES));
+$keys = array_shift($file);
+foreach ($file as $i=>$row) {
+    $row = array_combine($keys, $row);
+    $app['catalog'][$row['universeUrlname']]['subuniverses'][$row['subuniverseUrlname']]=$row;
+}
+
+$file = array_map("str_getcsv", file("https://docs.google.com/spreadsheets/d/1s10qJviUHayRFRHxSbMGNDKaIg7-gyYAjz6kOPhPm6g/pub?gid=1183165030&single=true&output=csv",FILE_SKIP_EMPTY_LINES));
+$keys = array_shift($file);
+foreach ($file as $i=>$row) {
+    $row = array_combine($keys, $row);
+    $app['catalog'][$row['universeUrlname']]['subuniverses'][$row['subuniverseUrlname']]['families'][$row['familyUrlname']]=$row;
+}
+
+$file = array_map("str_getcsv", file("https://docs.google.com/spreadsheets/d/1s10qJviUHayRFRHxSbMGNDKaIg7-gyYAjz6kOPhPm6g/pub?gid=1976579302&single=true&output=csv",FILE_SKIP_EMPTY_LINES));
+$keys = array_shift($file);
+foreach ($file as $i=>$row) {
+    $row = array_combine($keys, $row);
+    $app['catalog'][$row['universeUrlname']]['subuniverses'][$row['subuniverseUrlname']]['families'][$row['familyUrlname']]['criterias'][$row['criteriaName']]=$row;
+}
+
 $app->get('/', function() use($app) {
+    header('Content-type: application/json');
+  die(json_encode($app['catalog'], true));
   $app['monolog']->addDebug('logging output.');
   return $app['twig']->render('index.twig');
 });
@@ -34,7 +64,6 @@ $app->get('/catalog', function() use($app) {
       $catalog[$row['universeUrlname']]['subuniverses'][$row['subuniverseUrlname']]=$row;
   }
 
-  $families = array();
   $file = array_map("str_getcsv", file("https://docs.google.com/spreadsheets/d/1s10qJviUHayRFRHxSbMGNDKaIg7-gyYAjz6kOPhPm6g/pub?gid=1183165030&single=true&output=csv",FILE_SKIP_EMPTY_LINES));
   $keys = array_shift($file);
   foreach ($file as $i=>$row) {
@@ -42,7 +71,6 @@ $app->get('/catalog', function() use($app) {
       $catalog[$row['universeUrlname']]['subuniverses'][$row['subuniverseUrlname']]['families'][$row['familyUrlname']]=$row;
   }
 
-  $subfamilies = array();
   $file = array_map("str_getcsv", file("https://docs.google.com/spreadsheets/d/1s10qJviUHayRFRHxSbMGNDKaIg7-gyYAjz6kOPhPm6g/pub?gid=1976579302&single=true&output=csv",FILE_SKIP_EMPTY_LINES));
   $keys = array_shift($file);
   foreach ($file as $i=>$row) {
