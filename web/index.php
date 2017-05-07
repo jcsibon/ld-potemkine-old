5e03b4fbe8857100648e357f2d7a9173880f5f09
Jean-Christophe Sibon <jcsibon@gmail.com>
2017-05-07 15:40:06 +0200

    using silex

--- web/index.php
+++ web/index.php
@@ -13,7 +13,7 @@ foreach ($file as $i=>$row) {
 }
 
 foreach($universes as $universe)
-  $app['catalog'][$universes['universeUrlname']]=$universe;
+  $app['catalog'][$universe['universeUrlname']]=$universe;
 
 $app->register(new Silex\Provider\MonologServiceProvider(), array(
   'monolog.logfile' => 'php://stderr',
