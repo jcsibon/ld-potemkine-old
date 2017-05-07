7028233481127dcd6e780038a9f3aad44658949e
Jean-Christophe Sibon <jcsibon@gmail.com>
2017-05-07 15:38:03 +0200

    using silex

--- web/index.php
+++ web/index.php
@@ -5,6 +5,16 @@ require('../vendor/autoload.php');
 $app = new Silex\Application();
 $app['debug'] = true;
 
+$universes = array();
+$file = array_map("str_getcsv", file("https://docs.google.com/spreadsheets/d/1s10qJviUHayRFRHxSbMGNDKaIg7-gyYAjz6kOPhPm6g/pub?gid=1971894571&single=true&output=csv",FILE_SKIP_EMPTY_LINES));
+$keys = array_shift($file);
+foreach ($file as $i=>$row) {
+    $universes[] = array_combine($keys, $row);
+}
+
+foreach($universes as $universe)
+  $app['catalog']=$universe;
+
 $app->register(new Silex\Provider\MonologServiceProvider(), array(
   'monolog.logfile' => 'php://stderr',
 ));
@@ -19,16 +29,8 @@ $app->get('/', function() use($app) {
 });
 
 $app->get('/catalog', function() use($app) {
-  $universes = array();
-  $file = array_map("str_getcsv", file("https://docs.google.com/spreadsheets/d/1s10qJviUHayRFRHxSbMGNDKaIg7-gyYAjz6kOPhPm6g/pub?gid=1971894571&single=true&output=csv",FILE_SKIP_EMPTY_LINES));
-  $keys = array_shift($file);
-  foreach ($file as $i=>$row) {
-      $universes[] = array_combine($keys, $row);
-  }
-
   header('Content-type: application/json');
-  foreach($universes as $universe)
-    die(json_encode( $universe, true ));
+  die($app['catalog']);
 });
 
 $app->run();
