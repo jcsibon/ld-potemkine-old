c251ce1abbb407f1141be8e3e838af713eff470b
Jean-Christophe Sibon <jcsibon@gmail.com>
2017-05-08 12:43:06 +0200

    using silex

--- web/index.php
+++ web/index.php
@@ -58,6 +58,10 @@ $app->get('/{universeUrlname}-CCU0000/', function($universeUrlname) use($app) {
   return $app['twig']->render('universe.twig');
 })->assert('universeUrlname', '[a-z\-]+');
 
+$app->get('/fenetres-CCU0000/fenetres-porte-fenetres-battantes-CCN0000/fenetres-portes-fenetres-pvc-CCN0000/', function() use($app) {
+  return $app['twig']->render('windows.twig');
+});
+
 $app->get('/{universeUrlname}-CCU0000/{subuniverseUrlname}-CCN0000/{familyUrlname}-CCN0000/', function($universeUrlname, $subuniverseUrlname, $familyUrlname) use($app) {
   return $app['twig']->render('family.twig');
 })->assert('universeUrlname', '[a-z\-]+')->assert('subuniverseUrlname', '[a-z\-]+')->assert('familyUrlname', '[a-z\-]+');
@@ -74,9 +78,6 @@ $app->get('article-{articleId}', function($articleId) use($app) {
   return $app['twig']->render('article.twig');
 });
 
-$app->get('/fenetres-CCU0000/fenetres-porte-fenetres-battantes-CCN0000/fenetres-portes-fenetres-pvc-CCN0000/', function() use($app) {
-  return $app['twig']->render('windows.twig');
-});
 
 $app->get('/configurateur', function() use($app) {
   return $app['twig']->render('configurateur.twig');
