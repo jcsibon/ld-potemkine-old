017f0827f07e74d00fa233d67019b5a99152759d
Jean-Christophe Sibon <jcsibon@gmail.com>
2017-05-08 10:09:15 +0200

    using silex

--- web/index.php
+++ web/index.php
@@ -33,14 +33,14 @@ foreach ($file as $i=>$row) {
     $row = array_combine($keys, $row);
     $catalog[$row['universeUrlname']]['subuniverses'][$row['subuniverseUrlname']]['families'][$row['familyUrlname']]=$row;
 }
-
+/*
 $file = array_map("str_getcsv", file("https://docs.google.com/spreadsheets/d/1s10qJviUHayRFRHxSbMGNDKaIg7-gyYAjz6kOPhPm6g/pub?gid=1976579302&single=true&output=csv",FILE_SKIP_EMPTY_LINES));
 $keys = array_shift($file);
 foreach ($file as $i=>$row) {
     $row = array_combine($keys, $row);
     $catalog[$row['universeUrlname']]['subuniverses'][$row['subuniverseUrlname']]['families'][$row['familyUrlname']]['criterias'][$row['criteriaName']]=$row;
 }
-
+*/
 $app['twig']->addGlobal('catalog', $catalog);
 
 $app->get('/', function() use($app) {
