6794382d716de8721e754592cf11ac878d6076f8
SIBON <J7552815@sg.net>
2017-06-12 22:06:53 +0200

    using silex

--- web/index.php
+++ web/index.php
@@ -95,8 +95,8 @@ $app->get('/{universeUrlname}-{universeUrlcode}/', function($universeUrlcode) us
 })->assert('universeUrlcode', 'CCU[0-9\-]+');
 
 $app->get('/{universeUrlname}-{universeUrlcode}/{subuniverseUrlname}-{subuniverseUrlcode}/{familyUrlname}-{familyUrlcode}/', function($universeUrlname, $subuniverseUrlname, $familyUrlname) use($app) {
-  if ($universeUrlname == 'fenetres' && $subuniverseUrlname == 'fenetres-portes-fenetres-baies-coulissantes')
-    return $app['twig']->render('windows.twig');
+  if (in_array($familyUrlcode, ['CCN688463','CCN688464','CCN688465','CCN688466']))
+    return $app['twig']->render(familyUrlcode.'.twig');
   else
     return $app['twig']->render('family.twig');
 })->assert('universeUrlname', '[a-z\-]+')->assert('universeUrlcode', 'CCU[0-9\-]+')->assert('subuniverseUrlname', '[a-z\-]+')->assert('subuniverseUrlcode', 'SCU[0-9\-]+')->assert('familyUrlname', '[a-z\-]+')->assert('familyUrlcode', 'CCN[0-9\-]+');
