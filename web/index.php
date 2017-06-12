27c5565246e94f954362f75c7efb8a6ba781dd26
SIBON <J7552815@sg.net>
2017-06-12 19:25:08 +0200

    using silex

--- web/index.php
+++ web/index.php
@@ -27,7 +27,7 @@ foreach ($file as $i=>$row) {
       $newrow[$key] = $row[$j];
     $catalog[]=$newrow;
 }
-die(print_r($newrow));
+die(print_r($catalog));
 
 
 $file = array_map("str_getcsv", file("https://docs.google.com/spreadsheets/d/1s10qJviUHayRFRHxSbMGNDKaIg7-gyYAjz6kOPhPm6g/pub?gid=1971894571&single=true&output=csv",FILE_SKIP_EMPTY_LINES));
