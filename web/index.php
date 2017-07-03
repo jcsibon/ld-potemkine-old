0e04fd3c3b130050137efbf1efafb3e41565f4bd
Jean-Christophe Sibon <jcsibon@gmail.com>
2017-07-03 14:20:16 +0200

    update windows

--- web/index.php
+++ web/index.php
@@ -109,7 +109,7 @@ $app->get('/{universeUrlname}-{universeUrlcode}/{subuniverseUrlname}-{subunivers
     foreach ($file as $row)
       $windows[$row[2]][$row[3]][$row[4]][$row[5]][] = array("idArticle" => $row[0], "nom" => $row[1], "materiau" => $row[2], "type" => $row[3], "ouverture" => $row[4], "longueur" => $row[5], "largeur" => $row[6], "prix" => $row[7], "idProduit" => $row[8]);
 
-    return $app['twig']->render($familyUrlcode.'.twig', array($windows));
+    return $app['twig']->render($familyUrlcode.'.twig', array("windows"=>$windows));
   }
   elseif (in_array($familyUrlcode, ['CCN688009', 'CCN0010']))
     return $app['twig']->render('CCN688009.twig');    
