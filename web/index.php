6bdf7622172efd9e4be159398ce74e08c72005a6
Jean-Christophe Sibon <jcsibon@gmail.com>
2017-07-03 13:48:56 +0200

    update index.php

--- web/index.php
+++ web/index.php
@@ -101,11 +101,12 @@ $app->get('/{universeUrlname}-{universeUrlcode}/{subuniverseUrlname}-{subunivers
   if (in_array($familyUrlcode, ['CCN1010','CCN0025','CCN688463','CCN688464','CCN688465','CCN688466']))
   {
 
+/*
     $file = array_map("str_getcsv", file("https://docs.google.com/spreadsheets/d/1s10qJviUHayRFRHxSbMGNDKaIg7-gyYAjz6kOPhPm6g/pub?gid=1525879562&single=true&output=csv",,FILE_SKIP_EMPTY_LINES));
 
 header('Content-Type: application/json');
 die(json_encode($file));
-
+*/
 
     return $app['twig']->render($familyUrlcode.'.twig');
   }
